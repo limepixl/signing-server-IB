630f9a711b38749f553d36522cd43f1a3b4e30e3
Stefan Ivanovski <limepixl.dev@protonmail.com>
2022-05-15 15:31:33 +0200

    Added ProxyServer project

---
+++ ProxyServer/ProxyServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProxyServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
