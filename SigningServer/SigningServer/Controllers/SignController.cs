95a4546a7012e68bc8a1d8e2a49af8062262e2d6
Marija.Trpcevska <marija.trpcevska@gmail.com>
2022-05-17 17:23:18 +0200

    RSAServerKeyPair functionalities

--- SigningServer/SigningServer/Controllers/SignController.cs
+++ SigningServer/SigningServer/Controllers/SignController.cs
@@ -1,17 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Cryptography;
+using ProxyServer.Data;
+using ProxyServer.Models;
+
 
 namespace ProxyServer.Controllers
 {
     public class SignController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        private readonly ILogger<HomeController> _logger;
+
+        static Helpers helper = new Helpers();
+        Boolean keys_generated = helper.KeyGeneration();
+
+        public SignController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        // [Authorize]
+        [Authorize]
         [HttpPost]
         public string RequestSignature(string content) {
 
@@ -22,7 +38,12 @@ namespace ProxyServer.Controllers
                 treba da go potpishe i vrati potpisot kako string (base64 oblik)
             
             */
-            
+            var UserId = _context.Users.Where(user => user.UserName == User.Identity.Name).SingleOrDefault().Id;
+            var Username = User.Identity.Name;
+            var Payload = String.Concat(content, UserId, Username);
+
+
+
             return "hahaahha";
         }
     }
