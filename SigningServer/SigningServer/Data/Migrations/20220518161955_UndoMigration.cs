1552aba1537c7ddc539098b311867bf99f1c46cd
Stefan Ivanovski <limepixl.dev@protonmail.com>
2022-05-18 18:20:13 +0200

    WIP

---
+++ SigningServer/SigningServer/Data/Migrations/20220518161955_UndoMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProxyServer.Data.Migrations
+{
+    public partial class UndoMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
